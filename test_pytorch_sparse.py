b56c7486e87c150cf63f2fef93d7df6fbf4400b7
ftmtk <ft.liao@mtkresearch.com>
2021-01-25 15:39:54 +0800

    Add tensor.matmul, torch.einsum

--- test_pytorch_sparse.py
+++ test_pytorch_sparse.py
@@ -4,8 +4,6 @@ import pandas as pd
 from timeit import Timer
 from collections import defaultdict
 
-from torch.cuda import is_available
-
 
 def _get_adjacent_matrix_rand(density: float = 0.1, nrows: int = 2048, ncols: int = 2048, dtype: torch.dtype = torch.int32):
     nedges = int(np.ceil(density * nrows * ncols))
@@ -21,12 +19,23 @@ def _test_sparse_matrix(number: int = 10, use_cuda: bool = False):
     def _torch_matmul(dense_A, dense_B):
         return torch.matmul(dense_A, dense_B)
 
+    def _tensor_matmul(dense_A, dense_B):
+        return dense_A.matmul(dense_B)
+
+    def _torch_einsum(dense_A, dense_B):
+        return torch.einsum("ik,kj->ij", dense_A, dense_B)
+
     def _torch_sparse_mm(sparse_A, dense_B):
         return torch.sparse.mm(sparse_A, dense_B)
 
+    dict_funcs = {'torch_matmul': _torch_matmul,
+                  'torch_einsum': _torch_einsum,
+                  'tensor_matmul': _tensor_matmul,
+                  'torch_sparse_mm': _torch_sparse_mm}
+
     device = torch.device('cuda' if use_cuda and torch.cuda.is_available() else 'cpu')
-    exp_results = {'dense_multiply': defaultdict(dict), 'sparse_multiply': defaultdict(dict)}
-    result = defaultdict(dict)
+    exp_results = {key: defaultdict(dict) for key in dict_funcs.keys()}
+    result = {key: defaultdict(dict) for key in dict_funcs.keys()}
 
     from_dim, to_dim = 8, 14
     decimals = 5
@@ -38,31 +47,36 @@ def _test_sparse_matrix(number: int = 10, use_cuda: bool = False):
             dense_adjmat = _get_adjacent_matrix_rand(density, n, n, torch.float32).to(device)
             sparse_adjmat = dense_adjmat.to_sparse().to(device)
 
-            timer_matmul = Timer(lambda: _torch_matmul(dense_adjmat, dense_matrix))
-            timer_sparse_mm = Timer(lambda: _torch_sparse_mm(sparse_adjmat, dense_matrix))
-            t_matmul = timer_matmul.timeit(number=number)
-            t_sparse_mm = timer_sparse_mm.timeit(number=number)
+            for key, func in dict_funcs.items():
+                timer = None
+                if 'sparse' not in key:
+                    timer = Timer(lambda: func(dense_adjmat, dense_matrix))
+                else:
+                    timer = Timer(lambda: func(sparse_adjmat, dense_matrix))
+                period = timer.timeit(number=number)
 
-            exp_results['dense_multiply'][n][decimal] = t_matmul
-            exp_results['sparse_multiply'][n][decimal] = t_sparse_mm
-            result[n][decimal] = t_matmul / t_sparse_mm
+                exp_results[key][n][decimal] = period
+            for key in ['torch_einsum', 'tensor_matmul', 'torch_sparse_mm']:
+                result[key][n][decimal] = exp_results['torch_matmul'][n][decimal] / exp_results[key][n][decimal]
 
-    df_matmul = pd.DataFrame.from_dict(exp_results['dense_multiply'])
-    df_sparse_mm = pd.DataFrame.from_dict(exp_results['sparse_multiply'])
-    df_dense_vs_sparse = pd.DataFrame.from_dict(result)
-    return df_matmul, df_sparse_mm, df_dense_vs_sparse
+    return exp_results, result
 
 
 if __name__ == "__main__":
-    for use_cuda in [True, False]:
+    for use_cuda in [True]:
         tag = f'{torch.cuda.get_device_name()}' if use_cuda and torch.cuda.is_available() else 'cpu'
         print('=' * 8 + f'{tag}' + '=' * 8)
 
-        df_matmul, df_sparse_mm, df_dense_vs_sparse = _test_sparse_matrix(20, use_cuda)
-        print(df_matmul.to_string())
-        print(df_sparse_mm.to_string())
-        print(df_dense_vs_sparse.to_string())
+        exp_results, result = _test_sparse_matrix(20, use_cuda)
 
-        df_matmul.to_csv(f'matmul_{tag}.csv')
-        df_sparse_mm.to_csv(f'sparse_mm_{tag}.csv')
-        df_dense_vs_sparse.to_csv(f'dense_vs_sparse_{tag}.csv')
+        dfs = {key: pd.DataFrame.from_dict(dic) for key, dic in exp_results.items()}
+        dfs_comp = {key: pd.DataFrame.from_dict(dic) for key, dic in result.items()}
+        for key, df in dfs.items():
+            print(f'======== {key}')
+            print(df.to_string())
+
+            df.to_csv(f"{key}_{tag}.csv")
+            if key != 'torch_matmul':
+                dfs_comp[key].to_csv(f'Comp_{key}_vs_torch_matmul_{tag}.csv')
+                print(f"=" * 16 + " compare with torch_matmul")
+                print(dfs_comp[key].to_string())
